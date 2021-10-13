0244a217d47cebea3963255710d8c594cb91e4da
Akash <akash.nara@smartsensesolutions.com>
2021-10-13 14:03:36 +0530

    Initial Commit

---
+++ CustomSegmentSwiftUI/CustomSegmentSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSegmentSwiftUIApp.swift
+//  CustomSegmentSwiftUI
+//
+//  Created by Akash on 13/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomSegmentSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
